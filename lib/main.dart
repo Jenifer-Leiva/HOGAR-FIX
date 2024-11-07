53734c81e30f0b098a67ca8dc02531c2160559f3
Danna <est.danna.londono@unimilitar.edu.co>
2024-11-06 23:43:21 -0500

    Base de Datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hogarfixapp/firebase_options.dart';
 
 Future<void> main() async {
+  // Asegúrate de que las bindings de Flutter estén inicializadas antes de iniciar Firebase.
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inicializa Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
+  
+  // Corre la aplicación
   runApp(const MainApp());
 }
 
@@ -23,4 +29,24 @@ class MainApp extends StatelessWidget {
       ),
     );
   }
+}
+
+// Función para agregar datos a Firestore
+void addDataToFirestore() async {
+  // Obtén una instancia de Firestore
+  final db = FirebaseFirestore.instance;
+  
+  // Define el nuevo documento que deseas agregar
+  final user = <String, dynamic>{
+    "Celular": 1815,
+    "Corre electronico": "Lovelace",
+    "Nombre": "Ada"
+  };
+
+  // Agrega el documento a la colección "users" y maneja la respuesta
+  db.collection("users").add(user).then((DocumentReference doc) {
+    print('DocumentSnapshot added with ID: ${doc.id}');
+  }).catchError((error) {
+    print("Error adding document: $error");
+  });
 }
